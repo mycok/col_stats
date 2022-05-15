111792665c61821febeee82d732d53b0b8239c70
myko <awesomeme155@gmail.com>
2022-05-16 00:37:28 +0300

    feat: add benchmarking tests for csvToFloat function
    - add benchmark tests
    - refactor csvToFloat function to improve cpu performance and memory usage

--- csv.go
+++ csv.go
@@ -25,17 +25,24 @@ func avg(data []float64) float64 {
 
 func csvToFloat(r io.Reader, column int) ([]float64, error) {
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 
 	column--
 
-	fileData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
-
 	var data []float64
 
-	for i, row := range fileData {
+	for i := 0; ; i++ {
+		row, err := cr.Read();
+		if err != nil {
+			// If there's no more rows to read from the file.
+			if err == io.EOF {
+				break
+			}
+
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
+		// Read and discard the first row of file.(this row normally contains titles).
 		if i == 0 {
 			continue
 		}
@@ -44,7 +51,7 @@ func csvToFloat(r io.Reader, column int) ([]float64, error) {
 		// having less columns in the row than the provided column argument makes the column
 		// argument invalid.
 		if len(row) <= column {
-			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+			return nil, fmt.Errorf("%w: file has only %d columns", ErrInvalidColumn, len(row))
 		}
 
 		// Try to convert row value for the provided column from a string to a float.
